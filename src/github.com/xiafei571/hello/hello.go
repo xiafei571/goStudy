027cc4ac383b389d99f83d52f6f99f3087fcfbe0
夏飞 <xiafei@tuotuonet.com>
2016-11-03 20:20:13 +0800

    冒泡排序，结果正确，不知道语法有没有更简洁的用法

--- src/github.com/xiafei571/hello/hello.go
+++ src/github.com/xiafei571/hello/hello.go
@@ -8,4 +8,6 @@ import (
 
 func main() {
 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println()
+	fmt.Printf("%v", stringutil.BubbleSort([]int{14, 12, 3, 9, 4, 5, 8}))
 }
---
+++ src/github.com/xiafei571/stringutil/bubbleSort.go
@@ -0,0 +1,12 @@
+package stringutil
+
+func BubbleSort(array []int) []int{
+	for i := 0; i < len(array); i++ {
+		for j := i + 1; j < len(array); j++ {
+			if array[i] > array[j] {
+				array[i], array[j] = array[j], array[i]
+			}
+		}
+	}
+	return array
+}
